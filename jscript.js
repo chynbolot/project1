6920b3de3888ff652bac6d783a86947190759d4f
unknown <example@gmail.com>
2021-02-05 00:56:47 +0600

    first commit

---
+++ jscript.js
@@ -0,0 +1,51 @@
+" use strict";
+
+// alert('hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer+5);
+
+
+// const answers = [];
+
+// answers[0] = prompt('wat is your name?','');
+// answers[1] = prompt('wat is your surname?','');
+// answers[2] = prompt('how old are you?','');
+
+
+// console.log(typeof(answers));
+
+//  const category = 'toys' ;
+//   console.log(`https://someurl.com/${category}/5`) ;
+
+//   const user = 'Chynbolot';
+//   alert(` Hello ,${user}`);
+
+
+
+ console.log('arr'+"-obj");
+
+ console.log(5+5*9 !== 40);
+
+ let incr = 10,
+     decr = 10;
+
+    //  ++incr;
+    //  --decr;
+
+
+    console.log(++incr);
+    console.log(--decr);
+
+
+    console.log(5%2);
+
+    console.log(2*4===8);
+
+        const isChecked = false,
+               isClose = false; 
+
+            console.log(isChecked || !isClose);
---
+++ script.js
@@ -0,0 +1,25 @@
+// " use strict " ;
+// let number = (4) ;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+
+// const persone = '5' ;
+// const bool = false;
+
+// // console.log(something);
+
+// let und ;
+// console.log(und);
+
+// const obj = {
+//     name: "Chynbolot",
+//     age: 19,
+//     isMarried: false
+// };
+
+// console. log(obj.name);
+
+// let arr =[ 'plump.png', 'orange.jpg', '6', 'apple.bmp', {}, []];
+// console.log(arr[5]) ;
